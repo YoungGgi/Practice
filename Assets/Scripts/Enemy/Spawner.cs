ccbcbb9bb7c737d71d5271e9d420b4b2879c0be0
YoungGgi <dragonjin0122@gmail.com>
2024-01-23 17:07:24 +0900

    몬스터 생성 위치

--- Assets/Scripts/Enemy/Spawner.cs
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    
+    [SerializeField]
+    private Transform[] spawnPoint;
+
+    float timer;
+
+
+    // 자식 오브젝트들 초기화
+    private void Awake() {
+        spawnPoint = GetComponentsInChildren<Transform>();
+    }
+    
     private void Update() 
     {
-        if(Input.GetButtonDown("Jump"))
+        timer += Time.deltaTime;
+
+        if(timer > 0.2f)
         {
-            GameManager.instance.GetPool.Get(1);
+            Spawn();
+            timer = 0;
         }
+        
+    }
+
+    private void Spawn()
+    {
+        GameObject enemy = GameManager.instance.GetPool.Get(Random.Range(0, 2));
+        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
     }
 }
