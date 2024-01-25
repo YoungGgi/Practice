d3689c10b87db07d713c703a5f18b22aa5874ea7
YoungGgi <dragonjin0122@gmail.com>
2024-01-25 16:27:00 +0900

    근접 무기 회전값 설정

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -32,6 +32,23 @@ public class Weapon : MonoBehaviour
                 
                 break;
         }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            LevelUp(20, 7);
+        }
+
+    }
+
+    public void LevelUp(float damage, int count)
+    {
+        this.damage = damage;
+        this.count += count;
+
+        if(id == 0)
+        {
+            Batch();
+        }
     }
 
     public void Init()
@@ -52,8 +69,26 @@ public class Weapon : MonoBehaviour
     {
         for(int i = 0; i < count; i++)
         {
-            Transform bullet = GameManager.instance.GetPool.Get(prefabID).transform;
-            bullet.parent = transform;
+            Transform bullet;
+            if(i < transform.childCount)
+            {
+                bullet = transform.GetChild(i);
+            }
+            else
+            {
+                bullet = GameManager.instance.GetPool.Get(prefabID).transform;
+                bullet.parent = transform;
+            }
+
+            // 로컬포지션, 로컬회전값 초기화
+            bullet.localPosition = Vector3.zero;
+            bullet.localRotation = Quaternion.identity;
+
+            // 근거리 무기 회전
+            Vector3 rotVec = Vector3.forward  * 360 * i / count;
+            bullet.Rotate(rotVec);
+            bullet.Translate(bullet.up * 1.5f, Space.World); // 자신의 위치에서 1.5 위로, 월드 방향으로 이동
+
             bullet.GetComponent<Bullet>().Init(damage, -1);  // -1은 무한을 의미
         }
     }
